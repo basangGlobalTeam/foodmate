4c1d293c3e37b15ce653e175457bfc00341782e0
ThienLV <ohmygodvt95@gmail.com>
2018-08-14 16:40:12 +0700

    edit auth layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if controller_name == "landing"
+      "landing"
+    elsif devise_controller?
+      "login"
+    else
+      "application"
+    end
+  end
 end

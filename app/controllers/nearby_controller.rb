48405caac74f5e8caa3333ee4a805a1e38d96a05
ThienLV <ohmygodvt95@gmail.com>
2018-08-20 13:39:25 +0700

    fix

--- app/controllers/nearby_controller.rb
+++ app/controllers/nearby_controller.rb
@@ -1,7 +1,19 @@
 class NearbyController < ApplicationController
   def index
     if params[:km].present?
-      @posts = Post.near([params[:lat], params[:lng]], params[:km], units: :km).page(params[:page]).per 1
+      @posts = Post.near([params[:lat], params[:lng]], params[:km], units: :km).page(params[:page]).per 10
     end
+    @top_likes = Post.joins(:likes)
+      .group("posts.id")
+      .having("count(likes.id) > 0")
+      .order("created_at desc")
+      .where(created_at: 1.months.ago..Time.zone.now.to_date)
+      .limit 10
+    @top_followers = User.joins(:follows)
+      .group("users.id")
+      .having("count(follows.user_follow_id) > 0")
+      .where(created_at: 1.months.ago..Time.zone.now.to_date)
+      .where.not(id: current_user.follow_ids)
+      .limit 10
   end
 end
